ee2dbd5433bdda065d32a2f714cc9fe69fa93c8d
Edilson Torres da Silva <edilsontor77@gmail.com>
2026-06-18 11:50:41 -0300

    update questions

--- data.js
+++ data.js
@@ -1,103 +1,103 @@
 export const questions = [
     {
-        q: "Quantos selos havia no livro?",
+        q: "Quantas trombetas serão tocadas em Apocalipse?",
         options: [
-            "70",
+            "6",
+            "7",
             "12",
-            "3",
-            "7"
-        ],
-        answer: 3
-    },
-    {
-        q: "O que João viu na abertura do primeiro selo?",
-        options: [
-            "Cavaleiro preto",
-            "Cavaleiro branco",
-            "Cavaleiro amarelo",
-            "Cavaleiro vermelho"
+            "3"
         ],
         answer: 1
     },
     {
-        q: "O que simboliza o cavaleiro branco?",
+        q: "Ao som de qual trombeta cairá uma chuva de pedra misturada com sangue, matando terça parte das árvores e vegetações?",
         options: [
-            "Guerra e sangue",
-            "Morte e inferno",
-            "Conquista e domínio",
-            "Fome e escassez"
+            "1° trombeta",
+            "2° trombeta",
+            "3° trombeta",
+            "4° trombeta"
+        ],
+        answer: 0
+    },
+    {
+        q: "Ao som de qual trombeta estrelas caem queimando sobre as águas, as deixando amargas e muitos morrerem ao beber dessas águas?",
+        options: [
+            "1° trombeta",
+            "2° trombeta",
+            "3° trombeta",
+            "5° trombeta"
         ],
         answer: 2
     },
     {
-        q: "O que João viu na abertura do segundo selo?",
+        q: "Ao som da 4° trombeta, o que foi ferido?",
         options: [
-            "Cavaleiro vermelho",
-            "Cavaleiro amarelo",
-            "Cavaleiro branco",
-            "Cavaleiro preto"
+            "Um terço das árvores e vegetais",
+            "Um terço dos animais",
+            "Um terço do sol, da lua e das estrelas",
+            "Um terço das águas e da terra"
         ],
-        answer: 0
+        answer: 2
     },
     {
-        q: "O que simboliza o cavaleiro vermelho?",
+        q: "O que saía do buraco do abismo da quinta trombeta?",
         options: [
-            "Amor e paz",
-            "Guerra e sangue",
-            "Morte e fome",
-            "Conquista e domínio"
-        ],
-        answer: 1
-    },
-    {
-        q: "Quantos anos durará a grande tribulação?",
-        options: [
-            "7",
-            "70",
-            "12",
-            "3"
-        ],
-        answer: 0
-    },
-    {
-        q: "O que houve no céu na abertura do sétimo selo?",
-        options: [
-            "Barulho dos anjos",
-            "Apareceram duas testemunhas",
-            "Os anjos adorarão a Deus",
-            "Silêncio no céu"
+            "Escorpiões",
+            "Cavalos",
+            "Leões",
+            "Gafanhotos"
         ],
         answer: 3
     },
     {
-        q: "O que simbolizava os 144 mil na visão de João?",
+        q: "Com o que eram parecidos os dentes desses gafanhotos?",
         options: [
-            "Testemunhas de Jeová",
-            "A quantidade de pessoas salvas",
-            "Multidão da tribo de Levi",
-            "Gentios e judeus de todas as nações que foram fiéis até a morte na grande tribulação"
+            "Com dentes de lobo",
+            "Com dentes de leão",
+            "Com dentes de onça",
+            "Com dentes de cachorro"
         ],
-        answer: 3
+        answer: 1
     },
     {
-        q: "Sobre a grande multidão (mártires), Cristo disse a João que lavaram as vestes no sangue do cordeiro. O que isso significa?",
+        q: "Por que o anjo pede para João comer o livro?",
         options: [
-            "Morreram por amor a Cristo",
-            "Fizeram sacrifícios",
-            "Ungiram com sangue de cordeiro suas vestes",
-            "Jogaram sortes"
+            "Para não esquecer as escrituras",
+            "Para cumprir o que foi escrito: guardar os mandamentos",
+            "Para entender o peso, o juízo e o sofrimento que a mensagem traz aos que a têm em seu ventre",
+            "Para esconder do inimigo"
+        ],
+        answer: 2
+    },
+    {
+        q: "Quem possivelmente eram as duas testemunhas?",
+        options: [
+            "Elias e Enoque",
+            "Elias e Moisés",
+            "Elias e Davi",
+            "Elias e Eliseu"
         ],
         answer: 0
     },
     {
-        q: "Por que houve silêncio no céu na abertura do último selo?",
+        q: "Quantos dias as duas testemunhas profetizarão na terra?",
         options: [
-            "Porque não havia mulheres por lá",
-            "Reverência e expectativa no que Deus iria fazer",
-            "Porque todos se entristeceram com a situação da Terra",
-            "Porque Cristo já havia finalizado seus planos"
+            "1160",
+            "1260",
+            "1560",
+            "1260"
         ],
-        answer: 1
+        answer: 3
+    },
+    {
+        q: "O que acontece com os moradores da terra quando as duas testemunhas morrem?",
+        options: [
+            "Festejam e trocam presentes",
+            "Sentimento de tristeza e dor",
+            "Realizam uma cerimônia oficial com honras de estado",
+            "Ficam em silêncio"
+        ],
+        answer: 0
     }
 ];
 
